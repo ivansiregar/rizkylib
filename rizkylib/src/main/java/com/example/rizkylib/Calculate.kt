a31986bea6e568014dba646fb1e841ca27fd858f
ivan siregar <ivan.siregar@gmail.com>
2021-05-19 12:11:51 +0700

    first commit on 2021-05-19

---
+++ rizkylib/src/main/java/com/example/rizkylib/Calculate.kt
@@ -0,0 +1,14 @@
+package com.example.rizkylib
+
+class Calculate {
+
+    companion object {
+        fun tambah(i1: Int, i2: Int): Int {
+            return (i1 + i2)
+        }
+
+        fun kali(i1: Int, i2: Int): Int {
+            return (i1 * i2)
+        }
+    }
+}
