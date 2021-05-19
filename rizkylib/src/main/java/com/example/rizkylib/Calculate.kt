c5196fe9647ed2bbb47d7ca2afd59e1a5fe9318f
ivan siregar <ivan.siregar@gmail.com>
2021-05-19 12:25:01 +0700

    second commit on 2021-05-19 for fun kurang()

--- rizkylib/src/main/java/com/example/rizkylib/Calculate.kt
+++ rizkylib/src/main/java/com/example/rizkylib/Calculate.kt
@@ -10,5 +10,9 @@ class Calculate {
         fun kali(i1: Int, i2: Int): Int {
             return (i1 * i2)
         }
+
+        fun kurang(i1: Int, i2: Int): Int {
+            return (i1 - i2)
+        }
     }
 }
